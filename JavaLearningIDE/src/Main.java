c961261c15134335a6826eda31052f8c1f0e50b4
Shadman Jamil <shadman.jamil@gmail.com>
2024-03-12 00:14:35 +0100

    updated format to add other topics sequentially

--- JavaLearningIDE/src/Main.java
+++ JavaLearningIDE/src/Main.java
@@ -2,17 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
+        //Hello World
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
 
-        // Primitive data Types
+        // Primitive Data Types
         System.out.println("Primitive Data Types");
         int age = 12;
         double percentage = 50.80;
@@ -24,11 +19,22 @@ public class Main {
         System.out.println(firstInitial);
         System.out.println(isActive);
 
+
+        // Reference Data Types
         System.out.println("Reference Data Types");
         String firstName = "Shadman";
         String lastName = "Jamil";
 
         System.out.println(firstName);
         System.out.println(lastName);
+
+
+        // For Loop
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+
     }
 }
